b356be3a3f1c9e1273f3be8ef65bf6b7922bd389
Julie Butler <butler@frib.msu.edu>
2018-07-18 10:19:02 -0400

    Updated based on needs of the Magnus transformation and the PEP8 style guide
    Need to find a way to connect files from different repositories
    together so do not need to keep copying files back and forth.

--- LinearAlgebra.py
+++ LinearAlgebra.py
@@ -1,104 +1,65 @@
-##########################################################################################
+#########################################################################
 # LinearAlgebra.py
 # Julie Butler
-# Version 1.0
+# Version 1.1
 # July 9th, 2018
 #
-# A colection of methods that perform linear algebra related calculations.  The code is
-# split into different classes by topic/subject.
-##########################################################################################
+# A colection of methods that perform linear algebra related calculations.  The methods are organized
+# alphabetically, with an outline of all methods given below.
+#########################################################################
 
-##########################################################################################
-# CLASSES:
-# quantumMechanics: methods that are especially perternate to quantum mechanics 
-#	calculations 
-# linearAlgebra: basic linear algebra calculations that are not included in numpy or that
-#	needed to be modified from the numpy versions
-##########################################################################################
-
+#########################################################################
+# Function Outline:
+# commutator (x, y): computes the commutator of the numpy arrays x and y.
+# nested_commutator (x, y, n): computes the nested commutator to the nth degree of the 
+# numpy arrays x and y
+#########################################################################
 ##################################################
 #
 #                 IMPORTS
 #
 ##################################################
-# Allows for the use of effecient arrays and must basic linear algebra calculations
+# Third-Party Imports
 import numpy as np 
-# Commonly used methods from the numpy library 
 from numpy import dot
 
 ##################################################
 #
-#              quantumMechanics
+#                  commutator
 #
-# Methods:
-# commutator: takes in two numpy arrays and returns the commutator of them, which is
-#	defined as AB-BA.
-# nestedCommutator: takes in two numpy arrays and an integer and performs the nested
-#	commutator [a, b]^(n), where [a, b]^(n) = [a, [a, b]^(n-1)] and 
-#	[a, b]^(0) = b
 ##################################################
+def commutator (x, y):
+	"""	
+	 Performs a commutator on the numpy arrays x and y.
+	Input:
+		x (a numpy array): The first array that the nested commutator is to be performed on.
+		y (a numpy array): Thesecond array that the nested commutator is to be performed on.
+	Output:
+		Unnamed (depends on input):  The results of performing the commutator on x and y  
+	"""
+	return dot (x, y) - dot (y, x)
 
-class quantumMechanics:
-	##################################################
-	#
-	#                   __init__
-	#
-	##################################################
-	def __init__ (self, array1, array2):
-		"""
-		Input:
-			array1 (a numpy array): one of the arrays to perform 
-				calculations on
-			array2 (a numpy array): the other array to perform calculations
-				on
-		Output:
-			None.
-			
-		Initializes the class level numpy arrays a and b to the inputted numpy
-		arrays.  Order matters in these calculations so the order of array1 and
-		array2 matter.
-		"""
-		self.a = array1
-		self.b = array2
-	
-	##################################################
-	#
-	#                  commutator
-	#
-	##################################################
-	def commutator (self):
-		"""
-		Input:
-			None.
-		Output:
-			Unnamed (a double): the result of the commutator being
-				performed on class level numpy arrays a and b
-		
-		Performs a commutator on the class level numpy arrays a and b, i.e
-		returns the value of AB - BA.
-		"""
-		return dot (a, b) - dot (b, a)
-	
-	##################################################
-	#
-	#              nestedCommutator
-	#
-	##################################################
-	def nestedCommutator (self, n):
-		"""
-		Input:
-			n (an integer): the number of times the nested commutator routine
-				is to be performed.
-		Output:
-			Unnamed (a double): The result of performing the nested commutator n
-				times on class level numpy arrays a and b.
-				
-		Performs a nested commutator with n iterations on teh class level numpy arrays
-			a and b.  For the nested commutator [a, b]^(n) = [a, [a, b]^(n-1)] and
-			[a, b]^(0) = b.
-		"""
-		if n == 0:
-			return b
-		else:
-			return commutator (a, nestedCommutator (a, b, n-1))
+##################################################
+#
+#              nested_commutator
+#
+##################################################
+def nested_commutator (x, y, n):
+	"""
+    Performs a nested commutator with n iterations on the numpy arrays x and y.
+	Input:
+		x (a numpy array):  The first array that the nested commutator is to be performed
+			on.
+		y (a numpy array): The second array that the nested commutator is to be performed
+			on.
+		n (an integer): the number of times the nested commutator routine
+			is to be performed.
+	Output:
+		Unnamed (depends on input): The result of performing the nested commutator n
+			times on class level numpy arrays x and y.
+	"""
+	if n == 0:
+		return y
+	else:
+		return commutator (x, nested_commutator (x, y, n-1))
 		
